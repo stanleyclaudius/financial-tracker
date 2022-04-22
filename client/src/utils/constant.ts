e6512d8d347515797ed7cf125ffaecdf070d74f4
Stanley Claudius <stanleyclaudiuss@gmail.com>
2022-04-22 15:07:12 +0700

    Push to heroku

---
+++ client/src/utils/constant.ts
@@ -0,0 +1,3 @@
+export const GOOGLE_CLIENT_ID = '339838492859-tasrm3s56o7nv6rrnhvjb8hndnctcu6c.apps.googleusercontent.com'
+export const FACEBOOK_APP_ID = '503364081415095'
+export const RAPID_API_KEY = '13ddc7523cmsh8f5f6a973f332d9p182a43jsn721946656356'

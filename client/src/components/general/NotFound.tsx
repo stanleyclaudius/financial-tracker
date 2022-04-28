f68d60ce2d14d5152a4a4613804deb41d1fbef98
Stanley Claudius <stanleyclaudiuss@gmail.com>
2022-04-28 13:10:57 +0700

    Redesign NotFound component

--- client/src/components/general/NotFound.tsx
+++ client/src/components/general/NotFound.tsx
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom"
+
 const NotFound = () => {
   return (
-    <div>Not found page goes here</div>
+    <div className='w-screen h-screen overflow-hidden flex items-center justify-center bg-primary text-white flex-col'>
+      <img src={`${process.env.PUBLIC_URL}/img/logo.png`} alt='Fintrack' />
+      <h1 className='text-2xl mt-5 font-medium'>Page Not Found</h1>
+      <Link to='/' className='bg-secondary rounded-md px-5 py-2 mt-7 hover:bg-accent transition-[background]'>Back to Home</Link>
+    </div>
   )
 }
 
